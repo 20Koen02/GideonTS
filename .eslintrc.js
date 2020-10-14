f53aa37f97c82a5d510dbabda4eb8dc242002e15
20Koen02 <mail@koen02.nl>
2020-10-14 19:28:59 +0200

    Base of the application

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    "airbnb-typescript/base"
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    "no-console": "off"
+  }
+};
