0e4b81c74b8db7a737714f933513a9cb6ea2d1ec
20Koen02 <mail@koen02.nl>
2020-10-14 19:28:59 +0200

    Base of the application

---
+++ src/Config.ts
@@ -0,0 +1,5 @@
+export const token = 'MzYzNjk5ODQyMjAwODk1NDg4.Wc-v3w.1vGpMiD_H83h0_OMTaC-9YwIAwU';
+export const prefix = '.';
+export const owners: string[] = ['255009837002260482'];
+export const primaryColor = '#2db7ff';
+export const testChannel = '765964163054436422';
