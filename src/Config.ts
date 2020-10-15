4060dca0c71b4183ec60c830900ba7d1b37aa9f7
20Koen02 <mail@koen02.nl>
2020-10-15 11:57:56 +0200

    moved token to .env (and regenerated token)

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
-export const token = 'MzYzNjk5ODQyMjAwODk1NDg4.Wc-v3w.1vGpMiD_H83h0_OMTaC-9YwIAwU';
-export const prefix = '.';
+export const token: string = process.env.TOKEN;
+export const prefix: string = '.';
 export const owners: string[] = ['255009837002260482'];
-export const primaryColor = '#2db7ff';
-export const testChannel = '765964163054436422';
+export const primaryColor: string = '#2db7ff';
+export const testChannel: string = '765964163054436422';
